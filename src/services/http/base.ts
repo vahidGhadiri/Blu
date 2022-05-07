9037070995fce2a7da7f245075e5ac79dfa26c0d
vahidGhadiri <whydrf@gmail.com>
2022-05-07 14:43:58 +0430

    feat: Add Base api repository

---
+++ src/services/http/base.ts
@@ -0,0 +1,39 @@
+// Types
+import type {AxiosInstance} from "axios";
+// Utils
+import axios from "axios";
+
+/**
+ * The Http Service which created by axios instance
+ */
+abstract class Base {
+    protected httpService: AxiosInstance
+
+
+    /**
+     * Create new instance of custom config
+     * @protected
+     */
+    protected constructor() {
+        this.httpService = axios.create({
+            timeout: Number(process.env.REACT_APP_HTTP_TIMEOUT),
+            baseURL: `${process.env.REACT_APP_BASE_URL}`,
+            headers: {
+                "Content_Type": "application/json"
+            }
+        })
+
+        this.ResponseInterceptor()
+    }
+
+    /**
+     *  Can Add Interceptors here if needed,
+     * This Method will be called after receiving each response
+     */
+    private ResponseInterceptor() {
+        axios.interceptors.response.use(
+            () => console.log("OK!"),
+            () => console.log("NOT OK!")
+        )
+    }
+}
