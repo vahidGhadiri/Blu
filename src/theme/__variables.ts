91b3ba1eb83b69ac4b2415390726556da87df93c
vahidGhadiri <whydrf@gmail.com>
2022-05-08 20:58:22 +0430

    chore: JS doc for theme

--- src/theme/__variables.ts
+++ src/theme/__variables.ts
@@ -1,11 +1,21 @@
+/**
+ * This Object is a color repository
+ */
 export const COLORS = {
+    //Blue colors
     BLUE_DARK: "#2459f1",
     BLUE_DARKER: "#2c4473",
+
+    //Simple colors
     WHITE: "#ffffff",
+    SIMPLE_BLACK: "#000000",
+
+    //Utils colors
     BORDER: "rgba(0, 0, 0, 0.05)",
     INCREMENT_ARROW: "#27b488",
     DECREMENT_ARROW: "#e74c3c",
+
+    //Text colors
     LIGHT_TEXT: "#91abbb",
     DARK_TEXT: "#4d6c7d",
-    SIMPLE_BLACK: "#000000"
 }
